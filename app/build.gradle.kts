2c81a628ff323426398c47296c6928d17ad4a489
yezixx <skylow917@daum.net>
2024-12-04 23:11:56 +0900

    Chore: 소켓 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("io.socket:socket.io-client:2.0.0")
 }
