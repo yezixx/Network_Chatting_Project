08b91d11e778978920d6a8b0ff045da57454fb9f
yezixx <skylow917@daum.net>
2024-12-05 13:45:42 +0900

    Chore: recyclerview 추가, socket.io 제거

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,5 +66,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation("io.socket:socket.io-client:2.0.0")
+    implementation ("androidx.recyclerview:recyclerview:1.2.1")
 }
