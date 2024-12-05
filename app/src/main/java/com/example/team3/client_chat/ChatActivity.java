b2b6334f34b1e95db7551ee9b97c6c454e31d33b
yezixx <skylow917@daum.net>
2024-12-05 15:02:35 +0900

    Fix: import 수정

--- app/src/main/java/com/example/team3/client_chat/ChatActivity.java
+++ app/src/main/java/com/example/team3/client_chat/ChatActivity.java
@@ -6,6 +6,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
